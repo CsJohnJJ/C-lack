1f196c034604cc540f8b33411db1c6026cfb6feb
John Jiang <csjohnjj@gmail.com>
2020-02-13 01:30:04 -0500

    finish most basic css for homepage

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -36,6 +36,65 @@ class Splash extends React.Component {
                     Already using C-lack? <Link className="already-signup" to="/login">Sign In</Link>
                     </p>
                 </div>
+
+                <div className="homepage-2-div">
+                    <h2 className="break-text">Break out of the inbox</h2>
+                    <p className="working-text">
+                        Working in channels gives everyone on your
+                        team a shared view of progress and purpose.
+                    </p>
+                    <iframe src='https://www.youtube.com/embed/EYqxQGmQkVw'
+                        frameBorder='0'
+                        allow='autoplay; encrypted-media'
+                        allowFullScreen
+                        title='slackvideo'
+                    />
+                </div>
+
+                <div className="homepage-3-div">
+                    <div>
+                        <img src="https://image.flaticon.com/icons/svg/1828/1828765.svg" className="shape-img"/>
+                        <h3>Conversations, organized</h3>
+                        <p>
+                            Instead of a single overstuffed inbox,
+                            conversations in Meep happen in dedicated
+                            spaces called channels.
+                        </p>
+                    </div>
+                    <div>
+                        <img src="https://image.flaticon.com/icons/svg/2476/2476148.svg" className="news-img"/>
+                        <h3>Get looped in, not out</h3>
+                        <p>
+                            Meep makes it simple to follow
+                            conversations or find important information
+                            in an easily searchable archive.
+                        </p>
+                    </div>
+                    <div>
+                        <img src="https://image.flaticon.com/icons/svg/1694/1694871.svg" className="hand-img"/>
+                        <h3>Give focus a chance</h3>
+                        <p>
+                            Unlike email, Meep lets you choose
+                            which conversations are most important
+                            — and which can wait.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="homepage-4-div">
+                    <a href="https://github.com/CsJohnJJ" className="github-link">Github</a>
+                    <a href="" className="linkedin-link">LinkedIn(no link)</a>
+                </div>
+
+
+                <div className="homepage-5-div">
+                    <p className="better-text">Choose a better way to work</p>
+                    <ul className="homepage-link">
+                        <p>Contact Me   </p>
+                        <Link className="try-c-link-bottom" to="/signup">TRY C-LACK</Link>
+                        <Link className="try-demo-link-bottom" to="/login">TRY DEMO</Link>
+                    </ul>
+                </div>
             </div>
         )
     }
