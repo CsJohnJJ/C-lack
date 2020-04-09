fe90d809a3e5b4f003ad0485371d47de43bdf12b
John Jiang <csjohnjj@gmail.com>
2020-04-08 22:54:59 -0400

    update channel create and delete

--- frontend/components/channels/channel_index.jsx
+++ frontend/components/channels/channel_index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ChannelIndexItem from './channel_index_item';
+import CreateChannelForm from "./create_channel_form_container";
 import { Link } from 'react-router-dom';
 
 
@@ -12,12 +13,14 @@ class ChannelIndex extends React.Component {
         this.props.fetchUserChannels(this.props.currentUser.id)
     }
 
-    
+    createCh(){
+        this.props.createChannel(channel)
+    };
 
     render() {
         let channels = []
         let channelList;
-// debugger
+
         if (this.props.channels){
             channels = this.props.channels
             channelList = Object.values(channels).map(channel => {    
@@ -31,34 +34,29 @@ class ChannelIndex extends React.Component {
                 )
             })
         }
-   
         return (
-
-            <div>
-                <div>
-                    <h3 className="channel-title">Channels</h3>
-                </div>
-                <ul className="channelname-ul">
-                    {channelList}
-                </ul>
-                <div className="whole-placeholder">
-                    <div >
-                        <h3 className="channel-title">Direct Messages</h3>
-                    </div>
-                    <ul className="dmname-ul">
-                        <li className="dm-placeholder">
-                            @ C-lackbot
-                        </li>
-                        <li className="dm-placeholder">
-                            @ John Doe
-                        </li>
-                        <li className="dm-placeholder">
-                            @ Greg, the TA
-                        </li>
-                    </ul>
-                </div>
-
+          <div>
+            <div className="title-create">
+              <h3 className="channel-title">Channels</h3>
+              <Link className="create-link" to="/main/channels/create">
+                <img
+                  className="create-button"
+                  src="https://www.iconsdb.com/icons/preview/white/plus-xxl.png"
+                />
+              </Link>
             </div>
+            <ul className="channelname-ul">{channelList}</ul>
+            <div className="whole-placeholder">
+              <div>
+                <h3 className="channel-title">Direct Messages</h3>
+              </div>
+              <ul className="dmname-ul">
+                <li className="dm-placeholder">@ C-lackbot</li>
+                <li className="dm-placeholder">@ John Doe</li>
+                <li className="dm-placeholder">@ Greg, the TA</li>
+              </ul>
+            </div>
+          </div>
         );
     };
 };
